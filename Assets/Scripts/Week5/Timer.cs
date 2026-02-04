bed34de4bf645a5dcbe122bf5caece9c9b6beee7
miquelan <miquelan@OA-S141-11>
2026-02-04 09:40:25 -0500

    Made a timer using sliders and fill

---
+++ Assets/Scripts/Week5/Timer.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public float timerValue = 0;
+    public float timerMaxValue = 12;
+    public Slider timerVisuals;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        timerVisuals.maxValue = timerMaxValue;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timerValue += Time.deltaTime;
+        if (timerValue > timerMaxValue)
+        {
+            timerValue = 0;
+        }
+
+        timerVisuals.value = timerValue;
+    }
+}
